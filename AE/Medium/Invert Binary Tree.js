3de094b06f32edf4491a63f7cb0b20725ba47be2
Derick Park <derickmpark@gmail.com>
2020-07-09 18:01:23 -0700

    AE-Invert Binary Tree

---
+++ AE/Medium/Invert Binary Tree.js
@@ -0,0 +1,17 @@
+// Write a function that takes in a Bianry Tree and inverts instanceof. In other words, the function should swap every left node in the tree for its corresponding right node.
+
+// Time: O(n) n number of nodes
+// Space: O(d) d number of depth
+
+function invertBinaryTree(tree) {
+
+  function inverseTree(node) {
+    var temp = node.right;
+    node.right = node.left;
+    node.left = temp;
+
+    node.left && inverseTree(node.left);
+    node.right && inverseTree(node.right);
+  }
+  inverseTree(tree)
+}
